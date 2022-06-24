9d6507636d9aeeb7b568ee8da6031b6a27393344
4nZwar3 <4nzwar3@gmail.com>
2022-06-24 12:48:46 -0500

    first commit

---
+++ src/main/java/net/rum/sagdi/Sagdi.java
@@ -0,0 +1,18 @@
+package net.rum.sagdi;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sagdi implements ModInitializer {
+    public static final String MOD_ID = "sagdi"; 
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+    
+}
---
+++ src/main/java/net/rum/sagdi/SagdiClient.java
@@ -0,0 +1,13 @@
+package net.rum.sagdi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SagdiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        
+        
+    }
+    
+}
